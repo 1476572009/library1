be38e2f2f9f7727129ddcced118c520e2abb2c1e
yz <1476572009@qq.com>
2019-08-19 17:42:10 +0800

    Initial commit

---
+++ src/main/java/org/lanqiao/taru/library/LibraryApplication.java
@@ -0,0 +1,18 @@
+package org.lanqiao.taru.library;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    private static final Logger logger = LoggerFactory.getLogger(LibraryApplication.class);
+    public static void main(String[] args) {
+        logger.info("SpringBoot开始加载");
+        SpringApplication.run(LibraryApplication.class, args);
+        logger.info("SpringBoot加载完毕");
+    }
+
+}
