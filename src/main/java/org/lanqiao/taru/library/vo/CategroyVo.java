d01f1b23b24a51718821e2d7b0caa1e0d1ebc9f7
lemon-g <1963404621@qq.com>
2019-08-24 09:34:29 +0800

    categoryvo

---
+++ src/main/java/org/lanqiao/taru/library/vo/CategroyVo.java
@@ -0,0 +1,106 @@
+package org.lanqiao.taru.library.vo;
+
+public class CategroyVo {
+    private String bookId;
+    private String bookCode;
+    private String bookName;
+    private String bookAuthor;
+    private String bookCategoryId;
+//    private String bookPublishTime;
+    private String bookPrice;
+    private String bookStockSize;
+    private String bookComment;
+    private String bookStatus;
+    private  String categroyName;
+
+    public String getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(String bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getBookCode() {
+        return bookCode;
+    }
+
+    public void setBookCode(String bookCode) {
+        this.bookCode = bookCode;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookCategoryId() {
+        return bookCategoryId;
+    }
+
+    public void setBookCategoryId(String bookCategoryId) {
+        this.bookCategoryId = bookCategoryId;
+    }
+
+//    public String getBookPublishTime() {
+//        return bookPublishTime;
+//    }
+//
+//    public void setBookPublishTime(String bookPublishTime) {
+//        this.bookPublishTime = bookPublishTime;
+//    }
+
+    public String getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(String bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public String getBookStockSize() {
+        return bookStockSize;
+    }
+
+    public void setBookStockSize(String bookStockSize) {
+        this.bookStockSize = bookStockSize;
+    }
+
+    public String getBookComment() {
+        return bookComment;
+    }
+
+    public void setBookComment(String bookComment) {
+        this.bookComment = bookComment;
+    }
+
+    public String getBookStatus() {
+        return bookStatus;
+    }
+
+    public void setBookStatus(String bookStatus) {
+        this.bookStatus = bookStatus;
+    }
+
+
+
+    public String getCategroyName() {
+        return categroyName;
+    }
+
+    public void setCategroyName(String categroyName) {
+        this.categroyName = categroyName;
+    }
+
+}
